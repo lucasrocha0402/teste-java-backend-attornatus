3482538434eac3bf822efd5ecca7a16576ade7dc
Lucas Rocha <lukasrocha0402@gmail.com>
2023-01-17 08:49:00 -0300

    Adicionando a classe application

---
+++ src/main/java/com/teste/avaliacaoattornatus/AvaliacaoattornatusApplication.java
@@ -0,0 +1,18 @@
+package com.teste.avaliacaoattornatus;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class AvaliacaoattornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication
+				.run(AvaliacaoattornatusApplication.class, args);
+	}
+
+}
